25a159abeda4f266daf2e9bf290cb06ffa21e84b
suyu-booth <suyu@chicagobooth.edu>
2025-04-24 01:59:36 +0000

    comments part2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,7 +10,7 @@ class CommentsController < ApplicationController
   # GET /comments/1/edit
   def edit
     respond_to do |format|
-      # format.html
+      format.html
       format.js 
     end
     
@@ -39,9 +39,11 @@ class CommentsController < ApplicationController
       if @comment.update(comment_params)
         format.html { redirect_to root_url, notice: "Comment was successfully updated." }
         format.json { render :show, status: :ok, location: @comment }
+        format.js
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
+        
       end
     end
   end
